bcdffb282e42c48401a6764558097020a79c1cd8
KuroVine9 <kurovine9@gmail.com>
2023-11-14 02:37:21 +0900

    자잘자잘

--- src/main/kotlin/com/kuro9/libraend/db/DBHandler.kt
+++ src/main/kotlin/com/kuro9/libraend/db/DBHandler.kt
@@ -4,20 +4,12 @@ import com.kuro9.libraend.db.type.BasicReturnForm
 import com.kuro9.libraend.db.type.UuidReturnForm
 import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 import java.sql.CallableStatement
 import java.sql.SQLException
 import java.sql.SQLTimeoutException
 import java.sql.Timestamp
-import java.sql.Types
 import java.sql.Types.INTEGER
-import java.sql.Types.VARCHAR
-import kotlin.jvm.Throws
 
 @Service
 class DBHandler {
@@ -32,7 +24,7 @@ class DBHandler {
 
     @Throws(SQLException::class, SQLTimeoutException::class)
     fun registerUser(id: Int, pw: String): BasicReturnForm {
-        if(!checkPWString(pw)) return BasicReturnForm(400, "Not Valid PW Form!")
+        if (!checkPWString(pw)) return BasicReturnForm(400, "Not Valid PW Form!")
         return SIGNUP_QUERY.query {
             with(it) {
                 setInt(1, id)
@@ -57,7 +49,7 @@ class DBHandler {
 
     @Throws(SQLException::class, SQLTimeoutException::class)
     fun userLogin(id: Int, pw: String): UuidReturnForm {
-        if(!checkPWString(pw))
+        if (!checkPWString(pw))
             return UuidReturnForm(
                 BasicReturnForm(
                     400, "Not Valid PW Form!"
@@ -78,13 +70,15 @@ class DBHandler {
                 val sessKey = getString(4)
 
                 UuidReturnForm(
-                    BasicReturnForm(code, when(getInt(3)) {
-                        200 -> "OK"
-                        401 -> "ID/PW not correct!"
-                        500 -> "DB Error"
-                        else -> throw IllegalStateException("userLogin")
-                    }),
-                    when(code) {
+                    BasicReturnForm(
+                        code, when (getInt(3)) {
+                            200 -> "OK"
+                            401 -> "ID/PW not correct!"
+                            500 -> "DB Error"
+                            else -> throw IllegalStateException("userLogin")
+                        }
+                    ),
+                    when (code) {
                         200 -> sessKey
                         else -> null
                     }
@@ -94,30 +88,32 @@ class DBHandler {
     }
 
     @Throws(SQLException::class, SQLTimeoutException::class)
-    fun libraryReservation(seatId: Int, startTime: Timestamp, sessId: String): BasicReturnForm
-        = LIBRARY_RESERVATION_QUERY.query {
-        with(it) {
-            setInt(1, seatId)
-            setTimestamp(2, startTime)
-            setString(3, sessId)
-            registerOutParameter(4, INTEGER)
+    fun libraryReservation(seatId: Int, startTime: Timestamp, sessId: String): BasicReturnForm =
+        LIBRARY_RESERVATION_QUERY.query {
+            with(it) {
+                setInt(1, seatId)
+                setTimestamp(2, startTime)
+                setString(3, sessId)
+                registerOutParameter(4, INTEGER)
 
-            executeUpdate()
+                executeUpdate()
 
-            val code = getInt(4)
+                val code = getInt(4)
 
-            BasicReturnForm(code, when(code) {
-                200 -> "OK"
-                400 -> "Not valid time!"
-                401 -> "Not valid cert!"
-                403 -> "You already have reservation!"
-                404 -> "Seat Not Found!"
-                409 -> "Already have reservation on selected time!"
-                500 -> "DB Error"
-                else -> throw IllegalStateException("libraryReserv.")
-            })
+                BasicReturnForm(
+                    code, when (code) {
+                        200 -> "OK"
+                        400 -> "Not valid time!"
+                        401 -> "Not valid cert!"
+                        403 -> "You already have reservation!"
+                        404 -> "Seat Not Found!"
+                        409 -> "Already have reservation on selected time!"
+                        500 -> "DB Error"
+                        else -> throw IllegalStateException("libraryReserv.")
+                    }
+                )
+            }
         }
-    }
 
     @Throws(SQLException::class, SQLTimeoutException::class)
     fun libraryLogout(sessId: String): BasicReturnForm = LIBRARY_LOGOUT_QUERY.query {
@@ -129,16 +125,17 @@ class DBHandler {
 
             val code = getInt(2)
 
-            BasicReturnForm(code, when(code) {
-                200 -> "OK"
-                500 -> "DB Error"
-                else -> throw IllegalStateException("libraryLogout")
-            })
+            BasicReturnForm(
+                code, when (code) {
+                    200 -> "OK"
+                    500 -> "DB Error"
+                    else -> throw IllegalStateException("libraryLogout")
+                }
+            )
         }
     }
 
-    private fun checkPWString(pw: String): Boolean
-        = Regex("^[a-zA-Z0-9!@#\$%^&*()_+-=]{1,15}$").matches(pw)
+    private fun checkPWString(pw: String): Boolean = Regex("^[a-zA-Z0-9!@#\$%^&*()_+-=]{1,15}$").matches(pw)
 
 
     private fun <T> String.query(action: (CallableStatement) -> T): T {
--- src/main/kotlin/com/kuro9/libraend/router/config/Config.kt
+++ src/main/kotlin/com/kuro9/libraend/router/config/Config.kt
@@ -2,14 +2,15 @@ package com.kuro9.libraend.router.config
 
 import jakarta.servlet.http.Cookie
 
-const val ROOT_PATH = "/iotcomm/"
+const val ROOT_PATH = "/iot-comm/"
 const val API_PATH = ROOT_PATH + "api/"
-const val COOKIE_SESS_KEY = "sess-id"
+const val COOKIE_SESS_KEY = "sess_id"
+
 fun getCookie(sessId: String): Cookie =
     Cookie(COOKIE_SESS_KEY, sessId).apply {
         domain = "localhost" //TODO 나중에 변경 필요
         path = "/"
-        maxAge = 6 * 60 * 60
+        maxAge = 6 * 60 * 60 // 6시간
         // secure = true
         // isHttpOnly = true
 
